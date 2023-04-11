b4e80d8f82c913e6c8545b96abc755d7095405ff
nznrlln <102247982+nznrlln@users.noreply.github.com>
2023-04-11 21:55:09 +0300

    Версия 0.1.5

--- SocialAppDemo/ProfileScreen/ProfileScreenView.swift
+++ SocialAppDemo/ProfileScreen/ProfileScreenView.swift
@@ -19,7 +19,10 @@ class ProfileScreenView: UIView {
     private let postsCountLabel: UILabel = {
         let label = UILabel()
         label.toAutoLayout()
+        label.numberOfLines = 2
         label.font = Fonts.interReg14
+        label.text = "10" + "\nposts"
+        label.textAlignment = .center
 
         return label
     }()
@@ -27,7 +30,10 @@ class ProfileScreenView: UIView {
     private let subscribersCountLabel: UILabel = {
         let label = UILabel()
         label.toAutoLayout()
+        label.numberOfLines = 2
         label.font = Fonts.interReg14
+        label.text = "999" + "\nsubscribers"
+        label.textAlignment = .center
 
         return label
     }()
@@ -35,7 +41,10 @@ class ProfileScreenView: UIView {
     private let subscriptionCountLabel: UILabel = {
         let label = UILabel()
         label.toAutoLayout()
+        label.numberOfLines = 2
         label.font = Fonts.interReg14
+        label.text = "333" + "\nsubscribtions"
+        label.textAlignment = .center
 
         return label
     }()
@@ -78,8 +87,8 @@ class ProfileScreenView: UIView {
         collectionView.showsHorizontalScrollIndicator = false
 
         collectionView.register(UserCollectionViewCell.self, forCellWithReuseIdentifier: UserCollectionViewCell.identifier)
-//        collectionView.dataSource = self
-//        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.delegate = self
 
         return collectionView
     }()
@@ -114,6 +123,7 @@ class ProfileScreenView: UIView {
                          subscribersCountLabel,
                          subscriptionCountLabel,
                          separatorView,
+                         photosCollectionView,
                          postsTableView)
     }
 
@@ -131,19 +141,53 @@ class ProfileScreenView: UIView {
         subscribersCountLabel.snp.makeConstraints { make in
             make.top.equalTo(profileHeaderView.snp.bottom).offset(35)
             make.centerX.equalToSuperview()
-
         }
 
         subscriptionCountLabel.snp.makeConstraints { make in
             make.top.equalTo(profileHeaderView.snp.bottom).offset(35)
             make.trailing.equalToSuperview().inset(26)
         }
-        separatorView.snp.makeConstraints { make in
 
+        separatorView.snp.makeConstraints { make in
+            make.top.equalTo(postsCountLabel.snp.bottom).offset(15)
+            make.leading.trailing.equalToSuperview().inset(26)
+            make.height.equalTo(0.5)
+        }
+
+        photosCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(separatorView.snp.bottom).offset(15)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(100)
+        }
+
+        postsTableView.snp.makeConstraints { make in
+            make.top.equalTo(photosCollectionView.snp.bottom).offset(15)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
         }
     }
 
+}
 
+extension ProfileScreenView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        6
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: UserCollectionViewCell.identifier, for: indexPath) as! UserCollectionViewCell
+
+        return cell
+    }
+
+
+}
+
+
+extension ProfileScreenView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 72, height: 68)
+    }
     /*
      // Only override draw() if you perform custom drawing.
      // An empty implementation adversely affects performance during animation.
@@ -151,151 +195,4 @@ class ProfileScreenView: UIView {
      // Drawing code
      }
      */
-
-//
-//    private lazy var doubleTapGR: UITapGestureRecognizer = {
-//        let gesture = UITapGestureRecognizer(target: self, action: #selector(doubleTapAction))
-//        gesture.numberOfTapsRequired = 2
-//
-//        return gesture
-//    }()
-//
-////    init(viewModel: ProfileViewModel) {
-////        self.viewModel = viewModel
-////        super.init(nibName: nil, bundle: nil)
-////        self.viewModel.showUserData()
-////    }
-//
-////    required init?(coder: NSCoder) {
-////        fatalError("init(coder:) has not been implemented")
-////    }
-//
-////    override func viewDidLoad() {
-////        super.viewDidLoad()
-////
-////        // Do any additional setup after loading the view.
-////        viewInitialSettings()
-////    }
-//
-////    override func viewWillAppear(_ animated: Bool) {
-////        super.viewWillAppear(animated)
-////
-////        self.navigationController?.isNavigationBarHidden = true
-////    }
-//
-//    private func viewInitialSettings() {
-//        navigationItem.title = "Profile".localizable
-//        navigationController?.navigationBar.backgroundColor = Palette.whiteAndBlack
-//        view.backgroundColor = Palette.profileBackground
-//
-//        setupSubviews()
-//        setupSubviewsLayout()
-//    }
-//
-//    private func setupSubviews() {
-//        tableView.addGestureRecognizer(doubleTapGR)
-//        view.addSubview(tableView)
-//    }
-//
-//    private func setupSubviewsLayout(){
-//        NSLayoutConstraint.activate([
-//            tableView.topAnchor.constraint(equalTo: view.topAnchor),
-//            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-//            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-//            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-//        ])
-//    }
-//
-//    @objc private func doubleTapAction() {
-//
-//        let point = doubleTapGR.location(in: tableView)
-//        guard let indexPath = tableView.indexPathForRow(at: point) else {
-//            debugPrint("no indexPath")
-//            return
-//        }
-//
-//        if indexPath.section == 1 {
-//            print("👏🏻👏🏻👏🏻")
-////            viewModel.didSavePost(post: viewModel.posts[indexPath.row])
-//        }
-//
-//    }
-//
 }
-//
-//// MARK: - UITableViewDataSource
-//
-//extension ProfileViewController: UITableViewDataSource {
-//
-//    func numberOfSections(in tableView: UITableView) -> Int {
-//        return 2
-//    }
-//
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        switch section {
-//        case 0:
-//            return 1
-//        default:
-//            return viewModel.posts.count
-//        }
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//
-//        switch indexPath.section {
-//        case 0:
-//            let cell = tableView.dequeueReusableCell(withIdentifier: PhotosTableViewCell.identifier, for: indexPath) as! PhotosTableViewCell
-//            cell.photosButtonTapAction = { [weak self] in
-//                self?.viewModel.showGallery()
-//            }
-//
-//            return cell
-//        default:
-//            let cell = tableView.dequeueReusableCell(withIdentifier: PostTableViewCell.identifier, for: indexPath) as! PostTableViewCell
-//            cell.setupCell(model: viewModel.posts[indexPath.row])
-//
-//            return cell
-//        }
-//
-//    }
-//}
-//
-//// MARK: - UITableViewDelegate
-//
-//extension ProfileViewController: UITableViewDelegate {
-//
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        UITableView.automaticDimension
-//    }
-//
-//
-//    // метод для хедера
-//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-//        switch section {
-//        case 0:
-//            let header = ProfileHeaderView()
-//            return header
-//        default:
-//            let header = UIView()
-//            return header
-//        }
-//    }
-//
-//    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-//        let footer = UIView()
-//        return footer
-//    }
-//
-//    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-//        return 0
-//    }
-//
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        tableView.deselectRow(at: indexPath, animated: true)
-//
-//        if indexPath.section == 0 {
-//            viewModel.showGallery()
-//        }
-//    }
-//}
-//
